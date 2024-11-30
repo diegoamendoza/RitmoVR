95273952f2ba3c8142dc86ac0822fd54168ecaa5
crocre1 <nico.alonso.ferrada@gmail.com>
2024-11-30 19:15:01 -0300

    avance

--- Assets/Scripts/NoteBehavior.cs
+++ Assets/Scripts/NoteBehavior.cs
@@ -6,11 +6,22 @@ public class NoteBehavior : MonoBehaviour
 
     public NoteData NoteData { get; private set; }
 
+    // Velocidad de movimiento de las notas
+    public float moveSpeed = 5f;
+
+    // Dirección en la que se moverán las notas
+    private Vector3 moveDirection = Vector3.forward;
+
+    private void Update()
+    {
+        MoveNote();
+    }
+
     public void Initialize(NoteData noteData)
     {
         NoteData = noteData;
         Debug.Log(noteData.color);
-        
+
         if (lineRenderer == null)
         {
             lineRenderer = gameObject.AddComponent<LineRenderer>();
@@ -30,5 +41,11 @@ public class NoteBehavior : MonoBehaviour
             lineRenderer.SetPosition(0, transform.position);
             lineRenderer.SetPosition(1, targetPosition);
         }
+    }
+
+    private void MoveNote()
+    {
+        // Mueve la nota continuamente hacia adelante
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
     }
 }
